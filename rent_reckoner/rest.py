4b04c7120d7ad0819d79d6c516c57b60bee39cda
reveland <reveland92@gmail.com>
2017-05-10 21:53:20 +0200

    add cors

--- rent_reckoner/rest.py
+++ rent_reckoner/rest.py
@@ -1,10 +1,10 @@
 import json
 from reckoner import RentReckoner
 from provider import DataProvider
-from flask import Flask
+import flask
 
 
-APP = Flask(__name__)
+APP = flask.Flask(__name__)
 
 DATA_PATH = "./rent_reckoner/data/"
 
@@ -16,12 +16,18 @@ RENT_RECKONER = RentReckoner(DATA_PROVIDER)
 def get_dept(habitant_id, resident_id):
     dept = "# %d #" % RENT_RECKONER.get_debt(
         habitant_id, DATA_PROVIDER.get_resident_by_id(habitant_id, resident_id))
-    return dept
+    resp = flask.Response(dept)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
 
 
 @APP.route("/habitations/<int:habitant_id>/bills")
 def get_bills(habitant_id):
-    return json.dumps(RENT_RECKONER.get_bills_to_ui(habitant_id), default=lambda o: o.__dict__)
+    bills = json.dumps(RENT_RECKONER.get_bills_to_ui(
+        habitant_id), default=lambda o: o.__dict__)
+    resp = flask.Response(bills)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
 
 
 if __name__ == "__main__":

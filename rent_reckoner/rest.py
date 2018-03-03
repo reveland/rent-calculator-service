dc7cee698c3d382bafbbc14ec12b5f9a3c0dea3d
Peter Reveland <riotr@protonmail.com>
2018-03-03 16:54:42 +0100

    add bills_to_ui/resident_to_ui endpoints

--- rent_reckoner/rest.py
+++ rent_reckoner/rest.py
@@ -11,31 +11,23 @@ DATA_PATH = "./rent_reckoner/data/"
 DATA_PROVIDER = DataProvider(DATA_PATH)
 RENT_RECKONER = RentReckoner(DATA_PROVIDER)
 
-@APP.route("/habitations/<int:habitant_id>/residents/<int:resident_id>/dept")
-def get_dept(habitant_id, resident_id):
-    dept = "# %d #" % RENT_RECKONER.get_debt(
-        habitant_id, DATA_PROVIDER.get_resident_by_id(habitant_id, resident_id))
-    resp = flask.Response(dept)
+def create_response(data):
+    resp = flask.Response(data)
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
-
-@APP.route("/habitations/<int:habitant_id>/bills")
-def get_bills(habitant_id):
-    bills = json.dumps(RENT_RECKONER.get_bills_to_ui(
-        habitant_id), default=lambda o: o.__dict__)
-    resp = flask.Response(bills)
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-    return resp
+@APP.route("/habitations/<int:habitant_id>/bills_to_ui")
+def get_bills_to_ui(habitant_id):
+    bills_to_ui = RENT_RECKONER.get_bills_to_ui(habitant_id)
+    bills_to_ui = json.dumps(bills_to_ui, default=lambda o: o.__dict__)
+    return create_response(bills_to_ui)
 
 
-@APP.route("/habitations/<int:habitant_id>/residents")
-def get_residents(habitant_id):
-    residents = json.dumps(RENT_RECKONER.get_residents_to_ui(
-        habitant_id), default=lambda o: o.__dict__)
-    resp = flask.Response(residents)
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-    return resp
+@APP.route("/habitations/<int:habitant_id>/residents_to_ui")
+def get_residents_to_ui(habitant_id):
+    residents_to_ui = RENT_RECKONER.get_residents_to_ui(habitant_id)
+    residents_to_ui = json.dumps(residents_to_ui, default=lambda o: o.__dict__)
+    return create_response(residents_to_ui)
 
 
 @APP.route("/habitations/<int:habitant_id>/update_depts")

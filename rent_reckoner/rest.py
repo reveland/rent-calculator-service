256f2531e5f100e25ea26aa2341795d63b0c2cb9
Peter Reveland <reveland92@gmail.com>
2018-03-03 20:30:57 +0100

    fix

--- rent_reckoner/rest.py
+++ rent_reckoner/rest.py
@@ -4,7 +4,7 @@ from provider_trello import DataProvider
 import flask
 from flask import request
 
-APP = flask.Flask(__name__)
+app = flask.Flask(__name__)
 
 DATA_PATH = "./rent_reckoner/data/"
 
@@ -16,41 +16,41 @@ def create_response(data):
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
-@APP.route("/habitations/<int:habitant_id>/bills_to_ui")
+@app.route("/habitations/<int:habitant_id>/bills_to_ui")
 def get_bills_to_ui(habitant_id):
     bills_to_ui = RENT_RECKONER.get_bills_to_ui(habitant_id)
     bills_to_ui = json.dumps(bills_to_ui, default=lambda o: o.__dict__)
     return create_response(bills_to_ui)
 
 
-@APP.route("/habitations/<int:habitant_id>/residents_to_ui")
+@app.route("/habitations/<int:habitant_id>/residents_to_ui")
 def get_residents_to_ui(habitant_id):
     residents_to_ui = RENT_RECKONER.get_residents_to_ui(habitant_id)
     residents_to_ui = json.dumps(residents_to_ui, default=lambda o: o.__dict__)
     return create_response(residents_to_ui)
 
 
-@APP.route("/habitations/<int:habitant_id>/bills")
+@app.route("/habitations/<int:habitant_id>/bills")
 def get_bills(habitant_id):
     bills = DATA_PROVIDER.get_bills(habitant_id)
     bills = json.dumps(bills)
     return create_response(bills)
 
 
-@APP.route("/habitations/<int:habitant_id>/residents")
+@app.route("/habitations/<int:habitant_id>/residents")
 def get_residents(habitant_id):
     residents = DATA_PROVIDER.get_residents(habitant_id)
     residents = json.dumps(residents)
     return create_response(residents)
 
 
-@APP.route("/habitations/<int:habitant_id>/update_depts")
+@app.route("/habitations/<int:habitant_id>/update_depts")
 def update_depts(habitant_id):
     RENT_RECKONER.update_debts(habitant_id)
     return "updated"
 
 
-@APP.route("/habitations/<int:habitant_id>/residents", methods=['POST'])
+@app.route("/habitations/<int:habitant_id>/residents", methods=['POST'])
 def add_resident(habitant_id):
     r = request.get_json()
 
@@ -58,7 +58,7 @@ def add_resident(habitant_id):
     return create_response(r)
 
 
-@APP.route("/habitations/<int:habitant_id>/bills", methods=['POST'])
+@app.route("/habitations/<int:habitant_id>/bills", methods=['POST'])
 def add_bill(habitant_id):
     b = request.get_json()
 
@@ -66,4 +66,4 @@ def add_bill(habitant_id):
     return create_response(b)
 
 if __name__ == "__main__":
-    APP.run()
+    app.run(debug=True)

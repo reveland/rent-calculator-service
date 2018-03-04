781cbeba098c101db3c2a64958928c58d09c8d5e
Peter Reveland <reveland92@gmail.com>
2018-03-04 20:35:12 +0100

    14: add merge rest endpoint

--- rent_reckoner/rest.py
+++ rent_reckoner/rest.py
@@ -3,11 +3,15 @@ import flask
 from flask import request
 from rent_reckoner.reckoner import RentReckoner
 from rent_reckoner.provider.google_sheet_provider import GoogleDataProvider
+from rent_reckoner.provider.provider_trello import TrelloDataProvider
 
 app = flask.Flask(__name__)
 
-DATA_PROVIDER = GoogleDataProvider()
-RENT_RECKONER = RentReckoner(DATA_PROVIDER)
+GOOGLE_DATA_PROVIDER = GoogleDataProvider()
+TRELLO_DATA_PROVIDER = TrelloDataProvider()
+RENT_RECKONER = RentReckoner(GOOGLE_DATA_PROVIDER)
+
+DATA_PROVIDERS = [GOOGLE_DATA_PROVIDER, TRELLO_DATA_PROVIDER]
 
 def create_response(data):
     resp = flask.Response(data)
@@ -20,48 +24,47 @@ def get_bills_to_ui(habitant_id):
     bills_to_ui = json.dumps(bills_to_ui, default=lambda o: o.__dict__)
     return create_response(bills_to_ui)
 
-
 @app.route("/habitations/<int:habitant_id>/residents_to_ui")
 def get_residents_to_ui(habitant_id):
     residents_to_ui = RENT_RECKONER.get_residents_to_ui(habitant_id)
     residents_to_ui = json.dumps(residents_to_ui, default=lambda o: o.__dict__)
     return create_response(residents_to_ui)
 
-
 @app.route("/habitations/<int:habitant_id>/bills")
 def get_bills(habitant_id):
-    bills = DATA_PROVIDER.get_bills(habitant_id)
+    bills = GOOGLE_DATA_PROVIDER.get_bills(habitant_id)
     bills = json.dumps(bills)
     return create_response(bills)
 
-
 @app.route("/habitations/<int:habitant_id>/residents")
 def get_residents(habitant_id):
-    residents = DATA_PROVIDER.get_residents(habitant_id)
+    residents = GOOGLE_DATA_PROVIDER.get_residents(habitant_id)
     residents = json.dumps(residents)
     return create_response(residents)
-
 
 @app.route("/habitations/<int:habitant_id>/update_depts")
 def update_depts(habitant_id):
     RENT_RECKONER.update_debts(habitant_id)
     return "updated"
 
-
 @app.route("/habitations/<int:habitant_id>/residents", methods=['POST'])
 def add_resident(habitant_id):
-    r = request.get_json()
+    resident = request.get_json()
 
-    added_resident = DATA_PROVIDER.add_resident(habitant_id, r['start'], r['end'], r['name'])
-    return create_response(r)
-
+    GOOGLE_DATA_PROVIDER.add_resident(habitant_id, resident['start'], resident['end'], resident['name'])
+    return create_response(resident)
 
 @app.route("/habitations/<int:habitant_id>/bills", methods=['POST'])
 def add_bill(habitant_id):
-    b = request.get_json()
+    bill = request.get_json()
 
-    added_bill = DATA_PROVIDER.add_bill(habitant_id, b['start'], b['end'], b['type'], b['amount'], b['paid_by'])
-    return create_response(b)
+    GOOGLE_DATA_PROVIDER.add_bill(habitant_id, bill['start'], bill['end'], bill['type'], bill['amount'], bill['paid_by'])
+    return create_response(bill)
+
+@app.route("/habitations/<int:habitant_id>/merge/<int:source_id>/<int:target_id>", methods=['POST'])
+def merge(habitant_id, source_id, target_id):
+    DATA_PROVIDERS[source_id].merge(habitant_id, DATA_PROVIDERS[target_id])
+    return 'merged'
 
 if __name__ == "__main__":
     app.run(debug=True)
